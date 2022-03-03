a44b183e5027fc6f885cada5a7653e47e1643340
Hadrien Froger <hadrien@octree.ch>
2022-03-03 11:51:32 +0000

    ci: fix nginx config

--- backend/src/https-boot.js
+++ backend/src/https-boot.js
@@ -3,10 +3,15 @@ const fs = require("fs");
 
 module.exports = async function (strapi) {
   if (process.env.NODE_ENV !== "development") return Promise.resolve();
+  const { PORT: strapiPort = "3000" } = process.env;
+  const httpsPort = Math.max(parseInt(strapiPort, 10) + 1, 1300);
   const options = {
     key: fs.readFileSync("./localhost-key.pem"),
     cert: fs.readFileSync("./localhost.pem"),
   };
-  https.createServer(options, strapi.server.app.callback()).listen(1338);
+  https.createServer(options, strapi.server.app.callback()).listen(httpsPort);
+  strapi.log.info("To access the https server ⚡️, go to:");
+  strapi.log.info(`https://localhost:${httpsPort}`);
+  strapi.log.info(`https://localhost:${httpsPort}/admin`);
   return Promise.resolve();
 };
