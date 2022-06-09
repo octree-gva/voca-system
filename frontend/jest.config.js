7da807ea0b8fd86f26a329e77104e409ba2b37ec
Hadrien Froger <e3k8y9i0k3s8o9x4@octreea17.slack.com>
2022-06-09 13:09:14 +0000

    ci: faster ci and add ci checks

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,5 @@
+const {defaults} = require('jest-config');
+
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   // Add more setup options before each test is run
@@ -5,6 +7,7 @@ const customJestConfig = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleDirectories: ['node_modules', '<rootDir>/', 'utils'],
   snapshotSerializers: ['@emotion/jest/serializer'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
 
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
